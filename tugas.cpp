2217ed1bd52798a7398d4933ca3b9ef8b0d1fb36
al20250140008 <alf80169@gmail.com>
2026-04-27 12:13:56 +0700

    Menambahkan method setData pada BelahKetupat

--- tugas.cpp
+++ tugas.cpp
@@ -6,3 +6,11 @@ class BelahKetupat {
 private:
     double d1, d2, sisi;
 
+public:
+    void setData(double pD1, double pD2, double pSisi) {
+        d1 = pD1;
+        d2 = pD2;
+        sisi = pSisi;
+    }
+
+    
