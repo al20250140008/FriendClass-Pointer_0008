98c7153c345e742fd058d4ccd12730d263d6b1d6
al20250140008 <alf80169@gmail.com>
2026-04-27 12:05:58 +0700

    Membuat class mahasiswa dan fungsi tampil nim

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -8,17 +8,4 @@ class mahasiswa {
     }
 };
 
-int main() {
-    mahasiswa mhs{1}; // Object mhs
-    mhs.showNim(); // Member Access Operator
 
-    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
-    refMhs.nim = 2; // Member Access Operator
-    refMhs.showNim();
-
-    mahasiswa *pMhs = &mhs; // Pointer Dereference pMhs
-    pMhs->nim = 3; // Arrow Operator
-    pMhs->showNim();
-
-    return 0;
-}
