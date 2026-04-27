cecdcbaa37eba8337ef65ae335ddc0a88de931e5
al20250140008 <alf80169@gmail.com>
2026-04-27 12:07:08 +0700

    Menambahkan penggunaan pointer pada class mahasiswa

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -16,4 +16,9 @@ int main() {
     refMhs.nim = 2; // Member Access Operator
     refMhs.showNim();
 
-    
+    mahasiswa *pMhs = &mhs; // Pointer Dereference pMhs
+    pMhs->nim = 3; // Arrow Operator
+    pMhs->showNim();
+
+    return 0;
+}
