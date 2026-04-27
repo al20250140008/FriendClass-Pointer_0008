9d4323000fca8ac1fdddf80953ab13a46e67ebd8
al20250140008 <alf80169@gmail.com>
2026-04-27 12:01:04 +0700

    Menambahkan struktur class siswa dengan atribut id dan deklarasi method

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -13,3 +13,13 @@ public:
     friend class siswa; // Memberikan akses ke class siswa
 };
 
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
