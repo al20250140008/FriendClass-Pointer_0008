1fb3bd9ebaade25904d03386a4cd8ea6d7d4c280
al20250140008 <alf80169@gmail.com>
2026-04-27 11:37:27 +0700

    Menambahkan class BelahKetupat dengan fungsi luas dan keliling

---
+++ tugas.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class BelahKetupat {
+private:
+    double d1, d2, sisi;
+
+public:
+    void setData(double pD1, double pD2, double pSisi) {
+        d1 = pD1;
+        d2 = pD2;
+        sisi = pSisi;
+    }
+
+    double hitungLuas() {
+        return 0.5 * d1 * d2;
+    }
+
+    double hitungKeliling() {
+        return 4 * sisi;
+    }
+
+    void tampilkan() {
+        cout << "===== Belah Ketupat =====" << endl;
+        cout << "Luas     : " << hitungLuas() << endl;
+        cout << "Keliling : " << hitungKeliling() << endl << endl;
+    }
