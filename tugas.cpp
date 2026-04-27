2135f085cf455ed1d48ebc0981878fcf5a0c1ee3
al20250140008 <alf80169@gmail.com>
2026-04-27 11:50:02 +0700

    Menambahkan fungsi main untuk menjalankan program

--- tugas.cpp
+++ tugas.cpp
@@ -27,3 +27,45 @@ public:
         cout << "Luas     : " << hitungLuas() << endl;
         cout << "Keliling : " << hitungKeliling() << endl << endl;
     }
+};
+
+class LayangLayang {
+private:
+    double d1, d2, s1, s2;
+
+public:
+    void setData(double pD1, double pD2, double pS1, double pS2) {
+        d1 = pD1;
+        d2 = pD2;
+        s1 = pS1;
+        s2 = pS2;
+    }
+
+    double hitungLuas() {
+        return 0.5 * d1 * d2;
+    }
+
+    double hitungKeliling() {
+        return 2 * (s1 + s2);
+    }
+
+    void tampilkan() {
+        cout << "===== Layang-Layang =====" << endl;
+        cout << "Luas     : " << hitungLuas() << endl;
+        cout << "Keliling : " << hitungKeliling() << endl << endl;
+    }
+};
+
+int main() {
+    // Menggunakan Object BelahKetupat
+    BelahKetupat bk;
+    bk.setData(10, 12, 7); // d1, d2, sisi
+    bk.tampilkan();
+
+    // Menggunakan Object LayangLayang
+    LayangLayang ll;
+    ll.setData(15, 20, 10, 13); // d1, d2, s1, s2
+    ll.tampilkan();
+
+    return 0;
+}
